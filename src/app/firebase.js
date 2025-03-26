47aec0374f2597d1a3a29a7269ca69e46ed46092
anstariq333 <anstariq333@gmail.com>
2025-03-26 13:25:17 +0500

    resolve .env issu

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -8,13 +8,13 @@ console.log('Loading Firebase with config:', {
 });
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyDQo35Q_emKjMYK4zd-zQ-pVXm2wwd51Lk",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "valetbookingapp.firebaseapp.com",
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL || "https://valetbookingapp-default-rtdb.firebaseio.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "valetbookingapp",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "valetbookingapp.appspot.com",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "582291223613",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:582291223613:web:2721c2b8a80ef140f2e654"
+  apiKey:  "AIzaSyDQo35Q_emKjMYK4zd-zQ-pVXm2wwd51Lk",
+  authDomain: "valetbookingapp.firebaseapp.com",
+  databaseURL:  "https://valetbookingapp-default-rtdb.firebaseio.com",
+  projectId:  "valetbookingapp",
+  storageBucket:"valetbookingapp.appspot.com",
+  messagingSenderId: "582291223613",
+  appId: "1:582291223613:web:2721c2b8a80ef140f2e654"
 };
 
 const app = initializeApp(firebaseConfig);
