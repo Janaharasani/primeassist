ce4824416258dca6fe4dcb45873115c16850171b
anstariq333 <anstariq333@gmail.com>
2025-04-09 00:32:56 +0500

    removed extra pages from the app

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,9 @@
-import ValetBookingForm from "./components/ValetBookingForm";
-import  Footer from "./components/Footer";
-import StadiumScene from '@/app/components/Stadium/StadiumScene';
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
 import Image from "next/image";
-import LayoutXPadding from "./components/LayoutXPadding"
-import Testimonials from "./components/Testimonials";
-import OurStory from "./components/OurStory";
+
 
 export default function Home() {  
-
-
   return (
        <div>
-          <div className="relative  pt-5">
-        <div className="absolute inset-0 -z-10">
           <Image
                       src={"/hero-background.png"}
                       alt="career-counseling Hero"
@@ -23,23 +12,6 @@ export default function Home() {
                       className="h-full w-full object-cover hidden sm:block"
                     />
         </div>
-
-        <LayoutXPadding>
-          <div className="relative z-[70]">
-            <Navbar />
-          </div>
-        </LayoutXPadding>
-
-        <Hero/>
-      </div>
-       <div className="hidden sm:block">
-       <StadiumScene  />
-       </div>
-       <OurStory/>
-        <Testimonials/>
-        <Footer/>
-        </div>
-
     
   );
 }
